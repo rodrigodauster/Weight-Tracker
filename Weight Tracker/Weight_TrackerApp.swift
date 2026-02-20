cae19fd1e84db7fc55d4d701a397f1fbc056e294
Rodrigo Dauster <rdauster@gmail.com>
2026-02-20 17:42:03 +0100

    Initial Commit

---
+++ Weight Tracker/Weight_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Weight_TrackerApp.swift
+//  Weight Tracker
+//
+//  Created by Rodrigo Dauster on 20/02/2026.
+//
+
+import SwiftUI
+
+@main
+struct Weight_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
